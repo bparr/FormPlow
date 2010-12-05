3a2326a3aad646cd80a4e82224071f5b7eaf47e6
Ben Parr <bparr@bparr.com>
2010-12-05 08:17:50 -0500

    Add prefs so that FormPlow data is synced by Firefox Sync

--- defaults/preferences/formplow.js
+++ defaults/preferences/formplow.js
@@ -1,4 +1,6 @@
 
 pref("extensions.formplow.whitelist", "");
+pref("services.sync.prefs.sync.extensions.formplow.whitelist", true);
 pref("extensions.formplow.profileNames", "");
+pref("services.sync.prefs.sync.extensions.formplow.profileNames", true);
 
